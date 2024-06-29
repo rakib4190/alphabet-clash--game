3d94ddbc5f131b90106892b98d6175b2a554cac5
Rakibul Islam <mrislam4141@gmail.com>
2024-06-29 13:25:09 +0600

    addEventListener added in the js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,33 @@
+function handleKeyboardKeyPress(event){
+    const playerPressed = event.key;
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+   const currentAlphabet = currentAlphabetElement.innerText.toLowerCase();
+   if(currentAlphabet === playerPressed){
+     // remove alphabet key  background color
+     removeBackgroundColorById(currentAlphabet);
+    continueGame();
+   
+   }
+   else{
+    console.log('your life is drcresed');
+   }
+   
+}
+document.addEventListener('keyup', handleKeyboardKeyPress);
+
+
+
+
 function continueGame(){
+    //generate a random alphabet
    const alphabet = getRaandomAlphabet();
    //set randomlly alphabet to the screen
    const currentAlphabetElement = document.getElementById('current-alphabet');
    currentAlphabetElement.innerText = alphabet; 
    // set alphabet key  background color
    setBackgroundColorById(alphabet);
+
+
 }
 
 function play(){
--- scripts/utility.js
+++ scripts/utility.js
@@ -22,3 +22,8 @@ function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+// set alphabet key background color
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
